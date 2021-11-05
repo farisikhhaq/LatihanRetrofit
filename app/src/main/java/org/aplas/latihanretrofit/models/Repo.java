18f50fefeae95983cd99980b6713d5ec7cb92c85
xalll <farisikhlasulhaq@yahoo.co.id>
2021-11-05 20:23:24 +0700

    Adding Repo

---
+++ app/src/main/java/org/aplas/latihanretrofit/models/Repo.java
@@ -0,0 +1,21 @@
+package org.aplas.latihanretrofit.models;
+
+public class Repo {
+
+    private Integer id;
+    private String htmlUrl;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
