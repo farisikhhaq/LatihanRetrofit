c0466dcd6840f18baa935b7f4972977adc05cfc3
xalll <farisikhlasulhaq@yahoo.co.id>
2021-11-05 20:22:43 +0700

    Adding MainActivity Chage Repo and GithubServices

--- app/src/main/java/org/aplas/latihanretrofit/MainActivity.java
+++ app/src/main/java/org/aplas/latihanretrofit/MainActivity.java
@@ -1,14 +1,52 @@
 package org.aplas.latihanretrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.aplas.latihanretrofit.databinding.ActivityMainBinding;
+
+import java.util.List;
+
+import org.aplas.latihanretrofit.models.Repo;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import org.aplas.latihanretrofit.services.GithubServices;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        GithubServices service = retrofit.create(GithubServices.class);
+        Call<List<Repo>> repos = service.listRepos("hbb.polinema");
+
+        repos.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> repoList) {
+                binding.setRepo (repoList.body().get(0));
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+                Log.e("Error",t.getMessage());
+            }
+        });
+     }
+
+    public void handleSend(View view) {
     }
 }
